86a275bba0d503ba6c3a82c018208a89cc2d9e93
danielwerg <35052399+danielwerg@users.noreply.github.com>
2019-02-28 16:04:55 +0300

    better way to get age
    :)

--- content.js
+++ content.js
@@ -1,31 +1,24 @@
-var date = document.querySelector('div[id="name-born-info"] > time').innerText;
+var rawDate = document.querySelector('div[id="name-born-info"] > time');
+if (rawDate) rawDate = rawDate.innerText;
+// console.log(rawDate);
 
-var d = new Date();
-var age = d.getFullYear() - date.slice(-5); // slice to get last 4
-age--;  // assume they haven't had birthday yet
+var birthDate = new Date(rawDate);
+var dateNow = new Date();
 
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var actorM = date.split(" ")[0];
-var actorMNr = months.indexOf(actorM);
-var actorD = date.split(" ")[1].slice(0, -1);  // remove comma at end
+var age = dateNow.getFullYear() - birthDate.getFullYear();
 
-// add 1 year if they've had birthday
-if(d.getMonth() > actorMNr){
-    age++;
-}else if(d.getMonth() == actorMNr && d.getDate() >= actorD){
-    age++;
-}
+if (dateNow.getMonth() < birthDate.getMonth()
+    || dateNow.getMonth() == birthDate.getMonth()
+    && dateNow.getDate() < birthDate.getDate()) age--;
+// console.log(age);
 
 // #################################################################
 
-if(!document.getElementById("name-death-info")){    // chech if actor alive
+if (!document.getElementById("name-death-info") && rawDate) {    // chech if actor alive
     var x = document.getElementById("name-born-info");
     var child = x.childNodes[4];
-    
+
     // only add "in" for people with a birth location
-    if(x.childNodes.length > 5){
-        child.nodeValue = "(age " + age + ") in ";
-    }else{
-        child.nodeValue = "(age " + age + ")";
-    }
+    if (x.childNodes.length > 5) child.nodeValue = `(age ${age}) in `;
+    else child.nodeValue = `(age ${age})`;
 }
